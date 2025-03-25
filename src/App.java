1a6ec96d049a6a1d8135f05b219674a4f44c553b
sarthaksahu03 <sarthaksahu333@gmail.com>
2025-03-25 19:57:44 +0530

    flappybird

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        int boardwidth = 360;
+        int boardheight = 640;
 
+        JFrame frame = new JFrame("FlappyBird");
+        frame.setVisible(true);
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
